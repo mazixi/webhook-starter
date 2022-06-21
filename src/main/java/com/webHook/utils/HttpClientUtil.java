e183e7f1fffc53fe22c71f6887e0e6bb290444b3
mazixi <qiexingqieyin@163.com>
2022-06-21 16:22:47 +0800

    update

--- src/main/java/com/webHook/utils/HttpClientUtil.java
+++ src/main/java/com/webHook/utils/HttpClientUtil.java
@@ -1,5 +1,6 @@
 package com.webHook.utils;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
@@ -26,6 +27,7 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.net.URI;
 import java.util.*;
+
 @Slf4j
 @SuppressWarnings("all")
 public class HttpClientUtil {
@@ -76,9 +78,6 @@ public class HttpClientUtil {
         return doGet(url, null, username, password);
     }
 
-    public static String doPost(String url, Map<String, String> param) {
-        return doPost(url, param, null);
-    }
 
     private final static CloseableHttpClient getBasicHttpClient(String username, String password) {
         // 创建HttpClientBuilder
@@ -96,51 +95,6 @@ public class HttpClientUtil {
         // HttpClient
         CloseableHttpClient closeableHttpClient = httpClientBuilder.build();
         return closeableHttpClient;
-    }
-
-    public static String doPost(String url, Map<String, String> param, Map<String, String> header) {
-        // 创建Httpclient对象
-        CloseableHttpClient httpClient = getBasicHttpClient("huanglei", "111111");
-        CloseableHttpResponse response = null;
-        String resultString = "";
-        try {
-            // 创建Http Post请求
-            HttpPost httpPost = new HttpPost(url);
-            // 创建header头信息
-            if (header != null) {
-                for (Map.Entry<String, String> entry : header.entrySet()) {
-                    httpPost.addHeader(entry.getKey(), entry.getValue());
-                }
-            }
-            // 创建参数列表
-            if (param != null) {
-                List<NameValuePair> paramList = new ArrayList<>();
-                for (String key : param.keySet()) {
-                    paramList.add(new BasicNameValuePair(key, param.get(key)));
-                }
-                // 模拟表单
-                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList, "utf-8");
-                httpPost.setEntity(entity);
-            }
-            // 执行http请求
-            response = httpClient.execute(httpPost);
-            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                response.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        return resultString;
-    }
-
-    public static String doPost(String url) {
-        return doPost(url, null);
     }
 
     public static String doPostJson(String url, String json) {
@@ -166,7 +120,7 @@ public class HttpClientUtil {
             try {
                 response.close();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
+                log.info("HttpClientUtil.doPostJson error:{}", JSON.toJSONString(e.getMessage()));
                 e.printStackTrace();
             }
         }
@@ -186,7 +140,7 @@ public class HttpClientUtil {
             // 添加header
             httpPost = (HttpPost) addHeaders(httpPost, headers);
             // 创建请求内容
-            log.info("json:{}",json);
+            log.info("json:{}", json);
             StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
             httpPost.setEntity(entity);
             // 执行http请求
@@ -211,7 +165,6 @@ public class HttpClientUtil {
 
     private static HttpRequestBase addHeaders(HttpRequestBase httpRequest, Map<String, String> headers) {
         // 设置请求头信息，鉴权
-//        httpRequest.setHeader("Authorization", "Bearer da3efcbf-0845-4fe3-8aba-ee040be542c0");
         httpRequest.addHeader("Authorization", "1655177766");
         for (Map.Entry<String, String> entry : headers.entrySet()) {
             httpRequest.addHeader(entry.getKey(), entry.getValue());
@@ -224,12 +177,6 @@ public class HttpClientUtil {
         // 为httpGet实例设置配置
         httpRequest.setConfig(requestConfig);
         return httpRequest;
-    }
-
-
-    public static void main(String[] args) {
-
-
     }
 
 }
